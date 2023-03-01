0b84fe5665c9860c5e1d9f394cf70037cb997c84
Rahul Hembrom <69256240+zammyk@users.noreply.github.com>
2023-03-01 21:55:44 +0530

    Add grid modifier button

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let container = document.querySelector('.etch-container');
 let toAdd = document.createDocumentFragment();
-for(let i = 0;i<16;i++){
-    let newRow = document.createElement('div');
-    newRow.id = 'row-'+i;
-    newRow.classList.add('row');
-    for(let j = 0;j<16;j++){
-        let newCell = document.createElement('div');
-        newCell.id = 'cell-'+i+'-'+j;
-        newCell.classList.add('cell');
-        newRow.appendChild(newCell);
+let squarePerSide = 20;
+let cells;
+
+function makeGrid(){
+    for(let i = 0;i<squarePerSide;i++){
+        let newRow = document.createElement('div');
+        newRow.id = 'row-'+i;
+        newRow.classList.add('row');
+        for(let j = 0;j<squarePerSide;j++){
+            let newCell = document.createElement('div');
+            newCell.id = 'cell-'+i+'-'+j;
+            newCell.classList.add('cell');
+            newRow.appendChild(newCell);
+        }
+        toAdd.appendChild(newRow);
     }
-    toAdd.appendChild(newRow);
+    container.appendChild(toAdd);
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((cell)=>{
+        cell.addEventListener('mousemove',changeColor);
+    });
 }
-container.appendChild(toAdd);
+
+function emptyContainer(){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 function changeColor(e){
     if(e.buttons == 1) {
@@ -21,27 +36,43 @@ function changeColor(e){
     }
 }
 
-function resetCells(e){
-    for(let i = 0;i<16;i++){
-        for(let j = 0;j<16;j++){
+function resetCells(){
+    for(let i = 0;i<squarePerSide;i++){
+        for(let j = 0;j<squarePerSide;j++){
             let cell = document.getElementById('cell-'+i+'-'+j);
-            console.log(cell.id);
             cell.classList.remove('black-colored-cell');
         }
     }
 }
 
-let cells = document.querySelectorAll('.cell');
+function updateGrid(){
+    let inputNum = prompt("Please enter number of squares per side");
+    if (inputNum != null && !isNaN(inputNum)) {
+        if(inputNum<1||inputNum>100)
+        {
+            alert("Number of Squares per side should be in the range: [0,100]!");
+        }
+        emptyContainer();
+        squarePerSide = inputNum;
+        makeGrid();
+    }
+    else {
+        alert("Invalid Input!");
+    }
+}
+
+makeGrid();
 let resetBtn = document.querySelector('#resetBtn');
 let colorBtn = document.querySelector('#colorBtn');
 let blackBtn = document.querySelector('#blackBtn');
 let shadeBtn = document.querySelector('#shadeBtn');
-let buttons = [resetBtn,colorBtn,blackBtn,shadeBtn];
+let squareCntBtn = document.querySelector('#squareCntBtn');
+let buttons = [resetBtn,colorBtn,blackBtn,shadeBtn,squareCntBtn];
 
 cells.forEach((cell)=>{
     cell.addEventListener('mousemove',changeColor);
 });
-resetBtn.addEventListener('click',resetCells);
+
 buttons.forEach((button)=>{
     button.addEventListener('mousedown',()=>{
         button.classList.add('button-pressed');
@@ -52,3 +83,6 @@ buttons.forEach((button)=>{
         button.classList.remove('button-pressed');
     });
 });
+
+resetBtn.addEventListener('click',resetCells);
+squareCntBtn.addEventListener('click',updateGrid);
